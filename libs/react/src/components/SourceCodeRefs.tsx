7dc822f8735296450c5c34e6b67de8b93ad3e8c3
Don.Hsieh <don.shieh@gmail.com>
2024-05-26 11:15:00 +0800

    add component: SourceCodeRefs

---
+++ libs/react/src/components/SourceCodeRefs.tsx
@@ -0,0 +1,41 @@
+import { ComponentProps, ReactNode, memo } from "react";
+
+export type ReferenceEntry = {
+  title: ReactNode;
+  description?: ReactNode;
+  link: string;
+};
+
+export type SourceCodeRefsProps = {
+  title?: ReactNode;
+  data: ReferenceEntry[];
+} & Omit<ComponentProps<"details">, "title">;
+
+function SourceCodeRefs({
+  title = <b>Source Code References</b>,
+  data,
+  ...rest
+}: SourceCodeRefsProps) {
+  return (
+    <details {...rest}>
+      <summary>{title}</summary>
+      <ul>
+        {data.map((x) => {
+          return (
+            <li style={{ marginBlock: -8 }}>
+              <div className="row-vcenter" style={{ gap: 8 }}>
+                <span style={{ fontWeight: 500 }}>{x.title}</span>
+                <span>
+                  (<a href={x.link}>link</a>)
+                </span>
+              </div>
+              <p>{x.description}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </details>
+  );
+}
+
+export default memo(SourceCodeRefs);
